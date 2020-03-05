c9c1d03b322b0ff609283a3fa0e23f66dadc2247
Atahar Uddin <AtaharUddin@tcadmins-MacBook-Air-5.local>
2020-03-05 14:08:18 +0000

    112 edited

--- exercise_112.py
+++ exercise_112.py
@@ -1,25 +1,30 @@
 # get user input
-
 user_input = ''
-while user_input != 'pineapplepen':
-    user_input = input('Number:\n')
-
+while user_input != 'pineapple':
+    user_input = input("Input?\n> ")
+    print(type(user_input))
+    # check if input is a number
     if user_input.isdigit():
+        # play the game
         user_input = int(user_input)
-
+        print(type(user_input))
+        # print all numbers from 1 to input_number
         for number in range(1, user_input + 1):
             if (number % 3 == 0) and (number % 5 == 0):
-                print('bizzbuzz')
+                print("bizzzzuu")
+            # if it is a multiple of 3 it returns bizz
             elif number % 3 == 0:
-                print('BIZZ')
+                print("bizz")
+            # if a multiple of 5 it return zzuu
             elif number % 5 == 0:
-                print('BUZZ')
+                print("zzuu")
+            # if a multiple of 3 and 5 it return bizzzzuu
             else:
                 print(number)
-
-    elif user_input == 'pineapplepen':
-        print('Thank you for playing. Goodbye!')
+    # else (input is not a number)
+        # invalid input - give feedback
+    elif user_input == 'pineapple':
+        # handle exiting game
+        print("Thank you for playing.")
     else:
-        print('Enter a valid number')
-
-
+        print("Please enter a valid input")
